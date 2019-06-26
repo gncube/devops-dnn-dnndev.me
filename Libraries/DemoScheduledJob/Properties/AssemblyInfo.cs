1a29e04c1e0ee5b7e30905afe785b22de5143672
Gerry Ncube <gerry@thomaslaker.com>
2019-06-26 18:42:37 +0100

    added library and renamed Gsn. to GSN.

---
+++ Libraries/DemoScheduledJob/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("DemoScheduledJob Libraries Extension for DNN")]
+[assembly: AssemblyDescription("Demo scheduled job built using template")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Gsn")]
+[assembly: AssemblyProduct("DemoScheduledJob Libraries Extension for DNN")]
+[assembly: AssemblyCopyright("Copyright 2019 Gsn")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("67947DA3-813A-4E80-B7BC-47F99396202F")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("01.00.00")]
+[assembly: AssemblyFileVersion("01.00.00")]
