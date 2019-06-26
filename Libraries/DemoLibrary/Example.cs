0dbda69f436b764c73ad0ca58cb77e24c618bd62
Gerry Ncube <gerry@thomaslaker.com>
2019-06-26 18:31:09 +0100

    installed Demo Library

---
+++ Libraries/DemoLibrary/Example.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Gsn.Libraries.DemoLibrary
+{
+	/// <summary>
+    /// ExampleClass - A sample library built using the Upendo Template
+    /// </summary>
+	public static class ExampleClass
+	{
+		public const string DEFAULT = "Yay! I exist!";
+	}
+}
