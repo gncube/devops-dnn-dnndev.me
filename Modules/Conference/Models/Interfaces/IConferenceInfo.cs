a95d3b54e69941520bfb3021f04ab6a8b69b3bfb
Gerald Ncube <gncube@gmail.com>
2019-12-28 11:25:40 +0000

    cleared repo

--- Modules/Conference/Models/Interfaces/IConferenceInfo.cs
+++ Modules/Conference/Models/Interfaces/IConferenceInfo.cs
@@ -6,12 +6,12 @@ namespace GSN.Modules.Conference.Models
     public interface IConferenceInfo
     {
         int ConferenceId { get; set; }
-int ModuleId { get; set; }
-string Title { get; set; }
-string Description { get; set; }
-int CreatedByUserId { get; set; }
-DateTime CreatedOnDate { get; set; }
-int LastUpdatedByUserId { get; set; }
-DateTime LastUpdatedOnDate { get; set; }
+        int ModuleId { get; set; }
+        string Title { get; set; }
+        string Description { get; set; }
+        int CreatedByUserId { get; set; }
+        DateTime CreatedOnDate { get; set; }
+        int LastUpdatedByUserId { get; set; }
+        DateTime LastUpdatedOnDate { get; set; }
     }
 }
