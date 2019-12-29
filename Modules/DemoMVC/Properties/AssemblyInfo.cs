c5217d7465cba0387132f8bb8d26eb5b748c8f11
Gerald Ncube <gncube@gmail.com>
2019-12-29 12:02:53 +0000

    clear repo

---
+++ Modules/DemoMVC/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("DemoMVC Modules Extension for DNN")]
+[assembly: AssemblyDescription("Demo mvc")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("GSN")]
+[assembly: AssemblyProduct("DemoMVC Modules Extension for DNN")]
+[assembly: AssemblyCopyright("Copyright 2019 GSN")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("BCCC1AD9-CBE6-4320-BAE5-D2B06D1C739C")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("01.00.00")]
+[assembly: AssemblyFileVersion("01.00.00")]
