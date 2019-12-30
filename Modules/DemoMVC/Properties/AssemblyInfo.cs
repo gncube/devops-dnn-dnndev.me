fc686b9a85c272e71befcf910b056b402536d62e
Gerald Ncube <gncube@gmail.com>
2019-12-30 11:09:58 +0000

    <foldername>GSN/DemoMVC</foldername>

--- Modules/DemoMVC/Properties/AssemblyInfo.cs
+++ Modules/DemoMVC/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("DemoMVC Modules Extension for DNN")]
-[assembly: AssemblyDescription("Second take")]
+[assembly: AssemblyDescription("MVC module 11th take")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("GSN")]
 [assembly: AssemblyProduct("DemoMVC Modules Extension for DNN")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("D3AF6C94-6BA0-4524-9E55-410F5DD61763")]
+[assembly: Guid("46088727-7F20-4F7D-A580-DB1835547D4C")]
 
 // Version information for an assembly consists of the following four values:
 //
